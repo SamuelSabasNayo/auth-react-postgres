8bb6b79acff42343c453ecc68b6f53f66870a3f0
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-04-07 18:46:02 +0200

    added CRUD operations

--- lead_manager_react_redux/leadmanager/leads/views.py
+++ lead_manager_react_redux/leadmanager/leads/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, viewsets, permissions
 
-# Create your views here.
+from leads.models import Lead
+from .serializers import LeadSerializer
+
+
+class LeadViewSet(generics.GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin):
+    queryset = Lead.objects.all()
+    serializer_class =  LeadSerializer
+    # permission_classes = [
+    # permissions.IsAuthenticated
+    # ]
+
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+
+        if id:
+            return self.retrieve(request)
+
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
